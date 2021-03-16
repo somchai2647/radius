62dbe21d17c7c2a468bea450ae9d850cc85b8b2e
Somchai <somchai.b@cvc.ac.th>
2021-03-16 11:44:51 +0700

    fix

--- connect.php
+++ connect.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $dsn = 'mysql:dbname=radius;host=127.0.0.1';
 $db_user = "root";
@@ -18,25 +17,4 @@ function query($sql, $array = array())
     return $q;
 }
 
-=======
-<?php
-$dsn = 'mysql:dbname=radius;host=127.0.0.1';
-$db_user = "root";
-$db_password = "";
-try {
-    $db = new PDO($dsn, $db_user, $db_password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo $e->getMessage();
-}
-
-function query($sql, $array = array())
-{
-    global $db;
-    $q = $db->prepare($sql);
-    $q->execute($array);
-    return $q;
-}
-
->>>>>>> e8b43c166621b7d3706832f51dc8993b156622c9
 // error_reporting(0);
