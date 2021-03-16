c4401d5b3974df1958043d41b4dbd705585163f3
Somchai <somchai.b@cvc.ac.th>
2021-03-17 00:23:36 +0700

    del trigger

--- radius.sql
+++ radius.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 16, 2021 at 05:36 PM
+-- Generation Time: Mar 16, 2021 at 06:22 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.7
 
